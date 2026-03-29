35f67b0eff5a85dac4d2aa8a25d4d2347857f3ec
nicolas1756 <nicolas88yeh@gmail.com>
2026-03-30 04:31:14 +0800

    origin change

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: ['  http://localhost:4200', 'https://search-engine-phi.vercel.app'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
