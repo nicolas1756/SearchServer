a27ecffa17f35d2f1ac29d424236bf18289ff6f3
nicolas1756 <nicolas88yeh@gmail.com>
2026-03-30 16:31:06 +0800

    added to cors config

--- src/brave/brave.service.ts
+++ src/brave/brave.service.ts
@@ -1,8 +1,7 @@
 /* eslint-disable prettier/prettier */
-import { BadGatewayException, InternalServerErrorException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
-import { AxiosError } from 'axios';
 import { firstValueFrom } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 
@@ -18,30 +17,17 @@ export class BraveService {
         const apiKey = this.configService.get<string>('BRAVE_API_KEY');
         const url = 'https://api.search.brave.com/res/v1/web/search';
 
-        if (!apiKey) {
-            throw new InternalServerErrorException('Missing BRAVE_API_KEY in environment variables.');
-        }
+        const { data } = await firstValueFrom(
+            this.httpService.get<unknown>(url, {
+                params: { q: query },
+                headers: {
+                    Accept: 'application/json',
+                    'Accept-Encoding': 'gzip',
+                    'X-Subscription-Token': apiKey,
+                },
+            }),
+        );
 
-        try {
-            const { data } = await firstValueFrom(
-                this.httpService.get<unknown>(url, {
-                    params: { q: query },
-                    headers: {
-                        Accept: 'application/json',
-                        'Accept-Encoding': 'gzip',
-                        'X-Subscription-Token': apiKey,
-                    },
-                }),
-            );
-
-            return data;
-        } catch (error) {
-            const axiosError = error as AxiosError<any>;
-            const detail =
-                axiosError.response?.data?.error?.detail ??
-                axiosError.message ??
-                'Unknown error from Brave API';
-            throw new BadGatewayException(`Brave search failed: ${detail}`);
-        }
+        return data;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://search-engine-phi.vercel.app'],
+    origin: ['http://localhost:4200', 'my-angular-app-5ba.pages.dev', 'https://search-engine-phi.vercel.app'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
